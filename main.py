c182b333eecf1f4635a290443b498c0235d73dc2
Stephen Otremba <sotremba007@gmail.com>
2020-06-24 15:39:12 -0700

    removed username and linked only exisitng google accounts

--- main.py
+++ main.py
@@ -27,14 +27,14 @@ def close_db(conn):
 def create_user_table():
     """Creates a 'users' table in our database IF ONE DOES NOT ALREADY EXIST.  Does nothing otherwise."""
     conn, c = open_db()
-    c.execute('''CREATE TABLE IF NOT EXISTS users (given_name text, family_name text, email text, state text, username text, password text);''')
+    c.execute('''CREATE TABLE IF NOT EXISTS users (given_name text, family_name text, email text, state text, password text, federated int);''')
     close_db(conn)
 
 
-def insert_user(given_name, family_name, email, state, username, password):
+def insert_user(given_name, family_name, email, state, password, federated=0):
     """Inserts a user into the 'users' table of the database"""
     conn, c = open_db()
-    c.execute('''INSERT INTO users VALUES (?,?,?,?,?,?);''', (given_name, family_name, email, state, username, password))
+    c.execute('''INSERT INTO users VALUES (?,?,?,?,?,?);''', (given_name, family_name, email, state, password, federated))
     close_db(conn)
 
 
@@ -46,52 +46,42 @@ def is_email_registered(email):
     return user_email != None
 
 
-def is_username_registered(username):
-    """Determines if a given username is already registered in our database"""
+def is_password_correct(email, password):
+    """Determines if a given password matches the stored password for a particular email. 
+    The email must be in the database"""
     conn, c = open_db()
-    user = c.execute('''SELECT * FROM users WHERE username=?;''', (username,)).fetchone()
-    close_db(conn)
-    return user != None
-
-
-def is_password_correct(username, password):
-    """Determines if a given password matches the stored password for a particular username. 
-    The username must be in the database"""
-    conn, c = open_db()
-    stored_password = c.execute('''SELECT password FROM users WHERE username=?;''', (username,)).fetchone()
+    stored_password = c.execute('''SELECT password FROM users WHERE email=?;''', (email,)).fetchone()
     close_db(conn)
     return password == stored_password[0]
 
 
-def get_first_name(username):
-    """Retrieves the given name of a user in our database with the given username.  Username must be in the db"""
+def get_first_name(email):
+    """Retrieves the given name of a user in our database with the given email.  Username must be in the db"""
     conn, c = open_db()
-    first_name = c.execute('''SELECT given_name FROM users WHERE username=?;''', (username,)).fetchone()
+    first_name = c.execute('''SELECT given_name FROM users WHERE email=?;''', (email,)).fetchone()
     close_db(conn)
     return first_name[0]
 
 
-def create_federated_user_table():
-    """Creates a 'federated_users' table in our database IF ONE DOES NOT ALREADY EXIST.  Does nothing otherwise."""
-    conn, c = open_db()
-    c.execute('''CREATE TABLE IF NOT EXISTS federated_users (given_name text, family_name text, email text, state text, username text);''')
-    close_db(conn)
-
-
-def insert_federated_user(given_name, family_name, email, state, username="NULL"):
-    """Inserts a user into the 'federated_users' table of the database"""
-    conn, c = open_db()
-    c.execute('''INSERT INTO federated_users VALUES (?,?,?,?,?);''', (given_name, family_name, email, state, username))
-    close_db(conn)
-
+def is_account_federated(email):
+    """Determines if a given email is associated with a federated account in our database. If the email is not registered, this 
+    method returns False"""
+    registered = is_email_registered(email)
+    if not registered:
+        return False
     
-def is_federated_email_registered(email):
-    """Determines if a given email is already registered in our federated database"""
     conn, c = open_db()
-    user_email = c.execute('''SELECT * FROM federated_users WHERE email=?;''', (email,)).fetchone()
+    federated = c.execute('''SELECT federated FROM users WHERE email=?;''', (email,)).fetchone()
     close_db(conn)
-    return user_email != None
+    return federated[0] == 1
 
+
+def make_user_federated(email):
+    """Make the account associated with the given email a federated account. The email must be registered in the database"""
+    conn, c = open_db()
+    c.execute('''UPDATE users SET federated=1 WHERE email=?;''', (email,))
+    close_db(conn)
+    
 
 @app.route('/', methods=['GET'])
 def home_page():
@@ -115,35 +105,21 @@ def register_new_user():
     lname = request.values.get('lname')
     email = request.values.get('email').lower()
     state = request.values.get('state')
-    username = request.values.get('usr')
     password = request.values.get('passwrd')
     #should really encrypt and salt passwords, but this is just a demo
     
     create_user_table()
     registered_email = is_email_registered(email)
-    registered_username = is_username_registered(username)
-    
-    if username == "NULL" or username == "":
-        #do not accept NULL or empty username. send them back to login/registration page
-        print("Reg Failed: NULL or empty username")
-        error_message = "Username cannot be NULL or empty"
-        return render_template('register.html', reg_error=error_message)
         
     if registered_email:
         #send them back to a login/registration page
         print("Reg Failed: Existing Email")
         error_message = "The provided email is already linked to a registred account. Please provide another email."
         return render_template('register.html', reg_error=error_message)
-    
-    if registered_username:
-        #send them back to a login/registration page
-        print("Reg Failed: Existing Username")
-        error_message = "The provided username is already linked to a registred account. Please provide another username."
-        return render_template('register.html', reg_error=error_message)
         
     else:
         print("Registering New User")
-        insert_user(fname, lname, email, state, username, password)
+        insert_user(fname, lname, email, state, password, federated=0)
         return render_template('account_success.html', name=fname, registration=str(True))
 
 
@@ -156,21 +132,21 @@ def display_login():
 
 @app.route('/login-user', methods=['POST'])
 def login_existing_user():
-    """Attempt to log in an existing user into the database if the given password and username are 
+    """Attempt to log in an existing user into the database if the given password and email are 
     consistent with the database"""
     print("Logging in User")
-    username = request.values.get('usr')
+    email = request.values.get('email').lower()
     password = request.values.get('passwrd')
     
     create_user_table()
-    registered_username = is_username_registered(username)
-    if not registered_username:
-        error_message = "The given username is not associated with any registered account. Please provide a registered username."
+    registered_email = is_email_registered(email)
+    if not registered_email:
+        error_message = "The given email is not associated with any registered account. Please provide a registered email."
         return render_template('login.html', login_error=error_message)
     
-    correct_password = is_password_correct(username, password)
+    correct_password = is_password_correct(email, password)
     if correct_password:
-        first_name = get_first_name(username)
+        first_name = get_first_name(email)
         return render_template('account_success.html', name=first_name, login=str(True))
     
     error_message = "The given password for the specified account is incorrect."
@@ -195,16 +171,16 @@ def handle_onetap():
 
     token = request.values.get('credential')
     user_info = verify_id_token(token, _CLIENT_ID)
-    print(str(user_info))
+
     if not user_info:
         return render_template('home.html', error="Google Sign In failed. The ID Token was invalid.")
     
-    email = user_info['email']
-    create_federated_user_table()
-    registered_fed = is_federated_email_registered(email)
+    email = user_info['email'].lower()
+    create_user_table()
     registered = is_email_registered(email)
+    federated = is_account_federated(email)
     
-    if registered_fed:
+    if federated:
         return render_template('account_success.html', name=user_info['given_name'], login=str(True))
     elif registered:
         error_message = ("The email associated with this Google account is already registered. " 
@@ -212,13 +188,7 @@ def handle_onetap():
         return render_template('link_existing_account.html', link_error=error_message)
     else:
         session['decoded_token'] = user_info
-        return render_template('new_googler.html')
-
-
-@app.route('/new-google-account', methods=['GET'])
-def display_google_registration():
-    """Display the registration page for a new Google account on the site"""
-    return render_template('register_googler.html')
+        return render_template('register_googler.html')
 
 
 @app.route('/register-googler', methods=['POST'])
@@ -226,49 +196,37 @@ def register_new_googler():
     """Register the new Google account in the federated database using the provided information"""
     user_info = session['decoded_token']
     state = request.values.get('state')
+    password = request.values.get('passwrd')
     first_name = user_info['given_name']
     last_name = user_info['family_name']
     email = user_info['email']
     
-    create_federated_user_table()
-    insert_federated_user(first_name, last_name, email, state)
+    create_user_table()
+    insert_user(first_name, last_name, email, state, password, federated=1)
     
     return render_template('account_success.html', name=first_name, registration=str(True))
-
-
-@app.route('/link-account', methods=['GET'])
-def display_account_link():
-    """Display the account link page for linking an existing account to a Google account"""
-    return render_template('link_existing_account.html')
 
 
 @app.route('/link-login', methods=['POST'])
 def link_existing_user():
     """Attempt to link an existing account to a Google account"""
-    username = request.values.get('usr')
+    email = request.values.get('email').lower()
     password = request.values.get('passwrd')
     
     create_user_table()
-    registered_username = is_username_registered(username)
-    if not registered_username:
-        error_message = "The given username is not associated with any registered account. Please provide a registered username."
+    registered_email = is_email_registered(email)
+    if not registered_email:
+        error_message = "The given email is not associated with any registered account. Please provide a registered email."
         return render_template('link_existing_account.html', link_error=error_message)
     
-    correct_password = is_password_correct(username, password)
+    correct_password = is_password_correct(email, password)
     if not correct_password:
         error_message = "The given password for the specified account is incorrect."
         return render_template('link_existing_account.html', link_error=error_message)
-
-    user_info = session['decoded_token']
-    state = request.values.get('state')
-    first_name = user_info['given_name']
-    last_name = user_info['family_name']
-    email = user_info['email']
     
-    create_federated_user_table()
-    insert_federated_user(first_name, last_name, email, state, username)
+    make_user_federated(email)
     
-    return render_template('account_success.html', name=first_name, link=str(True))
+    return render_template('account_success.html', name=get_first_name(email), link=str(True))
 
 
 def verify_id_token(token, client_id):
